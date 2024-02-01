d013c5537dbb3bad10824ce2eafea9104223cf5d
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-02-01 16:22:01 -0500

    used spread syntax to change shallow copy to deep copy

--- quiz/scripts/map-deep.js
+++ quiz/scripts/map-deep.js
@@ -5,12 +5,18 @@ const originalMatrix = [
     [7, 9, 8]
   ];
   
-  const newMatrix = originalMatrix.map((row) => {
-    return row.sort();
+  const newMatrix = originalMatrix.map((row) => { 
+    // note: map returns SHALLOW COPY! If each element in the array is an obj, it maintains the reference to the array, not to the new objects!
+    
+    // return row.sort(); // fix this error by using Spread syntax instead. 
+
+    // or we can make new list instead by using SPREAD SYNTAX!!!
+    return [...row].sort();
   });
   
   // Modifying the original matrix (changing the last element of the first row)
-  originalMatrix[0][2] = 99;
+  originalMatrix[0][2] = 99; // changes the copy and references as well
+  
   
   // Displaying the original and new matrices
   console.log('Original Matrix (modified):', originalMatrix);
