c7760b8ae501d6baf1ab2f993540128548ca734c
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-02-05 11:36:43 -0500

    updated manipulatedom.js to add second column with EditText

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -21,12 +21,33 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+// create button to EditText in table (KS Feb.5)
+function createBtnNode(btnTxt, e, l) {
+  let btnNode = document.createElement("button");
+  let btnNodeText = createTxtNode(btnTxt);
+  btnNode.appendChild(btnNodeText);
+  btnNode.addEventListener(e, l);
+  return btnNode;
+}
+
 // TO DO STILL: See git stashed version of out-of-date main 
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    // add second col to table for edit field button (KS Feb. 5)
+    let col2 = createTDNode(createBtnNode("Edit text", "click", function() {
+      edit(col1);
+    }));
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
+}
+
+// create function to edit the text (KS Feb. 5)
+function edit(node) {
+  let newNode = document.createElement("input");
+  newNode.type = 'text';
+  newNode.placeholder = 'Enter cell (x,y) ...';
+  node.replaceChild(newNode, node.childNodes[0]);
 }
